706eaf0e19e2c19f3954215b107316d85aa3dd77
vascofg <vascofg@gmail.com>
2012-05-21 20:04:16 +0100

    Added first activity

--- fuel-monitor/src/org/feup/fuelmonitor/FuelMonitor.java
+++ fuel-monitor/src/org/feup/fuelmonitor/FuelMonitor.java
@@ -1,11 +1,11 @@
 package org.feup.fuelmonitor;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
 
 public class FuelMonitor extends Activity {
 	/** Called when the activity is first created. */
@@ -15,52 +15,17 @@ public class FuelMonitor extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		mDbHelper = new FuelMonitorDbAdapter(this);
-		openMainMenu();
-		// mDbHelper.open();
-	}
-
-	@Override
-	protected void onStop() {
-		super.onStop();
-		mDbHelper.close();
-	}
-
-	@Override
-	public void onBackPressed() {
-		openMainMenu();
-	}
-	
-	private void openMainMenu() {
 		setContentView(R.layout.main);
 		Button b1 = (Button) findViewById(R.id.fuelingButton);
-
+		final Context c = this;
 		b1.setOnClickListener(new View.OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
-				openInsertFueling();
+				Intent i = new Intent(c, AddFueling.class);
+				startActivity(i);
+
 			}
 		});
-
-	}
-
-	private void openInsertFueling() {
-		setContentView(R.layout.addfueling);
-		Spinner s = (Spinner) findViewById(R.id.brandSpinner);
-		Spinner s2 = (Spinner) findViewById(R.id.fuelTypeSpinner);
-		Spinner s3 = (Spinner) findViewById(R.id.yearSpinner);
-		String[] array_spinner = new String[2];
-		array_spinner[0] = "Olá";
-		array_spinner[1] = "Peru";
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_item, array_spinner);
-		ArrayAdapter<Integer> addVehicleYearAdapter = new ArrayAdapter<Integer>(
-				this, android.R.layout.simple_spinner_item);
-		for (int y = 2012; y >= 1900; y--)
-			addVehicleYearAdapter.add(y);
-		s.setAdapter(adapter);
-		s2.setAdapter(adapter);
-		s3.setAdapter(addVehicleYearAdapter);
 	}
 
 }
