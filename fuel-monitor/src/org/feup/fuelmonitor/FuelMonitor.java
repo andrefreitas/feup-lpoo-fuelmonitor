72c8199e31fdb5fe9f0b5d15fc507ddaee2c3b31
vascofg <vascofg@gmail.com>
2012-05-21 18:50:40 +0100

    Add fueling adicionado

--- fuel-monitor/src/org/feup/fuelmonitor/FuelMonitor.java
+++ fuel-monitor/src/org/feup/fuelmonitor/FuelMonitor.java
@@ -2,15 +2,33 @@ package org.feup.fuelmonitor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class FuelMonitor extends Activity {
-    /** Called when the activity is first created. */
+	/** Called when the activity is first created. */
 	private FuelMonitorDbAdapter mDbHelper;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        mDbHelper = new FuelMonitorDbAdapter(this);
-        mDbHelper.open();
-    }
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.addfueling);
+		mDbHelper = new FuelMonitorDbAdapter(this);
+		mDbHelper.open();
+		Spinner s = (Spinner) findViewById(R.id.brandSpinner);
+		Spinner s2 = (Spinner) findViewById(R.id.fuelTypeSpinner);
+		Spinner s3 = (Spinner) findViewById(R.id.yearSpinner);
+		String[] array_spinner = new String[2];
+		array_spinner[0] = "Olá";
+		array_spinner[1] = "Peru";
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, array_spinner);
+		ArrayAdapter<Integer> addVehicleYearAdapter = new ArrayAdapter<Integer>(
+				this, android.R.layout.simple_spinner_item);
+		for(int y=2012;y>=1900;y--)
+			addVehicleYearAdapter.add(y);
+		s.setAdapter(adapter);
+		s2.setAdapter(adapter);
+		s3.setAdapter(addVehicleYearAdapter);
+	}
 }
